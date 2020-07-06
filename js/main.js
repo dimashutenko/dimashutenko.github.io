77c4846b79a4f6979cddd969fe8f3836cb198bf8
Dima Shutenko <dima.shutenko.official@gmail.com>
2020-07-06 15:45:22 +0300

    6

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 $(document).ready( function() {
 
-    const btn_prev =$('#btn-prev');
-    const btn_next =$('#btn-next');
+    const btn_prev = $('#btn-prev'),
+    btn_next = $('#btn-next'),
+    slides = document.querySelectorAll('.slide'),
+    slides_wrapper = $('.slider-wrapper');
+
+    let index=0;
+
+    const activeSlide = n => {
+        for (slide of slides){
+            slide.classList.remove('active');
+        }
+        slides[n].classList.add('active');
+    }
+
+    const nextSlide = () => {
+        if(index == slides.length - 1){
+            index = 0;
+            activeSlide(index);
+        } else {
+            index++;
+            activeSlide(index);
+        }
+    }
+
+    btn_next.addEventListener('click', nextSlide);
 
 
 
